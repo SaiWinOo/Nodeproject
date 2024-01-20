e334bfcf9b1211ed44f3bc3f0ff5c648f73d17cc
SaiWinOo <saiwinoo52@gmail.com>
2024-01-20 11:38:59 +0630

    done

--- index.js
+++ index.js
@@ -15,21 +15,21 @@ const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cors({
-  origin: 'http://localhost:3000',
-}))
+// app.use(cors({
+//   origin: '*',
+// }))
+
 const PORT = process.env.PORT || 4000;
 
-app.get('/', (req, res) => {
-  return res.json({ message: 'hello world' })
-})
 
 app.use('/auth', authRoutes)
 app.use('/users', auth.verifyUserToken, userRoutes);
 app.use('/todo', todoRoutes)
 app.use('/message', auth.verifyUserToken, messageRoutes);
 
-
+app.get('/', (req, res) => {
+  return res.json({ message: 'hello world' })
+})
 
 mongoose.connect(db, {
 }).then(res => {
